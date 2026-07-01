8f2e19eccca7b99401e63f168031184a5a646ebd
Amelie Freeman <amelie@amelierosalyn.com>
2026-07-01 09:37:18 +0100

    Fixed validation error in seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ lawyer = User.find_or_create_by!(email: "lawyer@example.com") do |user|
   user.role = :lawyer
 end
 
-Question.find_or_create_by!(title: "Can my landlord increase my rent?") do |question|
+Question.find_or_create_by!(title: "Example question") do |question|
   question.user = client
-  question.body = "My landlord wants to increase my rent with very little notice. What are my options?"
-  question.category = "Housing"
+  question.body = "I have a question. How should I proceed?"
+  question.category = "housing"
   question.status = :open
 end
